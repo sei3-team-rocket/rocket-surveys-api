4e9f5c38ab50e98b7dcdd82c8129e4be142c5b6e
MTaylor124 <matthew_taylor1@student.uml.edu>
2019-08-08 10:58:14 -0400

    Added Dev branch and started models for answer

---
+++ app/models/survey.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const surveySchema = new mongoose.Schema({
+
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Survey', surveySchema)
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
+const surveyRoutes = require('./app/routes/survey_routes')
 
 // require error handling middleware
 const errorHandler = require('./lib/error_handler')
@@ -60,7 +61,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // register route files
 app.use(exampleRoutes)
 app.use(userRoutes)
-
+app.use(surveyRoutes)
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
 // passed any error messages from them
